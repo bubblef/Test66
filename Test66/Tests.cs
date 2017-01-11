8b07cab72078dd14b68b09eeb4e05b5b15aa36aa
Katya Kulikova <katya.kulikova@jetbrains.com>
2017-01-11 16:46:54 +0300

    Initial commit

---
+++ Test66/Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test66{
+    [TestFixture]
+    public class Tests{
+        [Test]
+        public void Test1(){
+            Assert.True (true);
+        }
+    }
+}
